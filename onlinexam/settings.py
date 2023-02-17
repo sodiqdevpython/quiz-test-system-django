a2d676e1ca40ce52b9b484a2122e4687a17d81e3
Sodiq <sodiq7898@gmail.com>
2023-02-18 00:33:50 +0500

    first commit2

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -18,7 +18,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -136,7 +135,7 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'from@gmail.com' 
+EMAIL_HOST_USER = 'from@gmail.com'
 EMAIL_HOST_PASSWORD = 'ENTER_PASSWORD' # host email password required
 # now sign in with your host gmail account in your browser
 # open following link and turn it ON
