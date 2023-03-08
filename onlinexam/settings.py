b170a713522ed2e1435ad6bd33466df681ea6716
Sodiq <sodiq7898@gmail.com>
2023-03-08 17:46:10 +0500

    add1

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -16,8 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
-MEDIA_URL = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/profile_pic/')
+MEDIA_URL = os.path.join(BASE_DIR, 'static/profile_pic/')
 
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
